4ad080c2794e1cfd25f6add7d9d4364aa4973303
Todd Smith <mail@toddbsmith.com>
2018-05-06 11:42:12 -0400

    formatting, fix IDE settings

--- word2vec.py
+++ word2vec.py
@@ -1,30 +1,23 @@
-""" The no frills implementation of word2vec skip-gram model using NCE loss.
-Author: Chip Huyen
-Prepared for the class CS 20SI: "TensorFlow for Deep Learning Research"
-cs20si.stanford.edu
-"""
-
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
-
+from processdata import process_data
+from tensorflow.contrib.tensorboard.plugins import projector
 import os
-os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
-
 import numpy as np
 import tensorflow as tf
-from tensorflow.contrib.tensorboard.plugins import projector
 
-from processdata import process_data
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
 VOCAB_SIZE = 50000
 BATCH_SIZE = 2560
-EMBED_SIZE = 2560 # dimension of the word embedding vectors
-SKIP_WINDOW = 1 # the context window
-NUM_SAMPLED = 64    # Number of negative examples to sample.
+EMBED_SIZE = 2560  # dimension of the word embedding vectors
+SKIP_WINDOW = 1  # the context window
+NUM_SAMPLED = 64  # Number of negative examples to sample.
 LEARNING_RATE = 1.0
 NUM_TRAIN_STEPS = 10000
-SKIP_STEP = 2000 # how many steps to skip before reporting the loss
+SKIP_STEP = 2000  # how many steps to skip before reporting the loss
+
 
 def word2vec(batch_gen):
     """ Build the graph for word2vec model and train it """
@@ -37,8 +30,8 @@ def word2vec(batch_gen):
     # Step 2: define weights. In word2vec, it's actually the weights that we care about
 
     with tf.name_scope('embedding_matrix'):
-        embed_matrix = tf.Variable(tf.random_uniform([VOCAB_SIZE, EMBED_SIZE], -1.0, 1.0), 
-                            name='embed_matrix')
+        embed_matrix = tf.Variable(tf.random_uniform([VOCAB_SIZE, EMBED_SIZE], -1.0, 1.0),
+                                   name='embed_matrix')
 
     # Step 3: define the inference
     with tf.name_scope('loss'):
@@ -46,39 +39,41 @@ def word2vec(batch_gen):
 
         # Step 4: construct variables for NCE loss
         nce_weight = tf.Variable(tf.truncated_normal([VOCAB_SIZE, EMBED_SIZE],
-                                                    stddev=1.0 / (EMBED_SIZE ** 0.5)), 
-                                                    name='nce_weight')
+                                                     stddev=1.0 / (EMBED_SIZE ** 0.5)),
+                                 name='nce_weight')
         nce_bias = tf.Variable(tf.zeros([VOCAB_SIZE]), name='nce_bias')
 
         # define loss function to be NCE loss function
-        loss = tf.reduce_mean(tf.nn.nce_loss(weights=nce_weight, 
-                                            biases=nce_bias, 
-                                            labels=target_words, 
-                                            inputs=embed, 
-                                            num_sampled=NUM_SAMPLED, 
-                                            num_classes=VOCAB_SIZE), name='loss')
+        loss = tf.reduce_mean(tf.nn.nce_loss(weights=nce_weight,
+                                             biases=nce_bias,
+                                             labels=target_words,
+                                             inputs=embed,
+                                             num_sampled=NUM_SAMPLED,
+                                             num_classes=VOCAB_SIZE), name='loss')
 
     # Step 5: define optimizer
     optimizer = tf.train.GradientDescentOptimizer(LEARNING_RATE).minimize(loss)
-    
+
     with tf.Session() as sess:
         sess.run(tf.global_variables_initializer())
 
-        total_loss = 0.0 # we use this to calculate late average loss in the last SKIP_STEP steps
+        total_loss = 0.0  # we use this to calculate late average loss in the last SKIP_STEP steps
         writer = tf.summary.FileWriter('./graphs/no_frills/', sess.graph)
         for index in range(NUM_TRAIN_STEPS):
             centers, targets = next(batch_gen)
-            loss_batch, _ = sess.run([loss, optimizer], 
-                                    feed_dict={center_words: centers, target_words: targets})
+            loss_batch, _ = sess.run([loss, optimizer],
+                                     feed_dict={center_words: centers, target_words: targets})
             total_loss += loss_batch
             if (index + 1) % SKIP_STEP == 0:
                 print('Average loss at step {}: {:5.1f}'.format(index, total_loss / SKIP_STEP))
                 total_loss = 0.0
         writer.close()
 
+
 def main():
     batch_gen = process_data(VOCAB_SIZE, BATCH_SIZE, SKIP_WINDOW)
     word2vec(batch_gen)
+
 
 if __name__ == '__main__':
     main()
